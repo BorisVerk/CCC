1f0f8d16f2463b14ed0624c881626f0f74c0d674
BorisVerk <boris.verk@gmail.com>
2013-12-19 06:39:03 -0700

    made 1.py 9 lines, because I can. Might make it 5

--- contests/2013/1.py
+++ contests/2013/1.py
@@ -1,10 +1,9 @@
-with open('s1.in', 'r') as infile:
-    year = int(infile.read()) + 1#in case given year is unique
-
 def next_unique(year):
     for digit in str(year):
         if str(year).count(digit) > 1:
             return next_unique(year + 1)
     else: return year
 
-print next_unique(year)
+with open('s1.in', 'r') as infile:
+    year = int(infile.read()) + 1 #the +1 is in case given year is unique
+print next_unique(year) 
