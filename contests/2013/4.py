e94e08c19539a275fd64c9be96bcc169a9c50154
BorisVerk <boris.verk@gmail.com>
2014-01-12 22:04:31 -0700

    added working solution to 4 (not mine)

--- contests/2013/4.py
+++ contests/2013/4.py
@@ -1,3 +1,59 @@
+# again, not my solution
+# note to self, rewrite this to learn to use nested lists
+
+# returns true if person_one is taller than person_two
+# uses a Breadth first Search thru the "search_tree" tree of size class_size
+def taller(search_tree, class_size, person_one, person_two):
+    # visited is a "visited" list for tracking (don't go to same guy twice)
+    visited = []
+    for r in range(class_size):
+        visited.append(False)    
+    
+    queue = []
+    for link in search_tree[person_one]:
+        queue.append(link)
+    while len(queue) > 0:
+        current_person = queue.pop(0)
+        if current_person == person_two:
+            return True
+        if not visited[current_person]:
+            visited[current_person] = True
+            for link in search_tree[current_person]:
+                queue.append(link)
+    return False
+         
+ 
+with open("s4.in", 'r') as infile:
+    line = infile.readline().strip().split(" ")
+    class_size = int(line[0])
+    total_comparisons = int(line[1])
+ 
+    # search_tree is the main array holding all the links
+    # it is a list of lists, where the index of the inner list
+    # is the person, and the elements withing that list
+    # are other people shorter than the index they are in
+    search_tree = []
+    for i in range(class_size):
+        row = []
+        search_tree.append(row)
+    
+    
+    for i in range(total_comparisons):
+        line = infile.readline().strip().split()
+        #the 1 is substracted to make a zero indexed array
+        person_one = int(line[0]) - 1 
+        person_two = int(line[1]) - 1
+        search_tree[person_one].append(person_two)
+     
+    line = infile.readline().strip().split()
+    person_one = int(line[0]) - 1
+    person_two = int(line[1]) - 1
+   
+if taller(search_tree, class_size, person_one, person_two): print "yes"
+elif taller(search_tree, class_size, person_two, person_one): print "no"
+else: print "unknown"
+    
+"""
 infile = open("/Users/HDD/src/CCC/contests/2013/s4.in", "r")
 
 first_line = infile.readline().split(" ")
@@ -39,3 +95,4 @@ def search(search_space):
     
     
 print search(search_space)
+"""
--- contests/2013/5.py
+++ contests/2013/5.py
@@ -1,19 +1,32 @@
-#not my solution
+#!/usr/bin/env python
+
+"""
+not my solution
+
+I feel like this solution has something to do with Euclid's algorithm for
+finding the GCD
+"""
+
 with open("s5.in", 'r') as infile:
     number = int(infile.readline().strip())
  
 cost = 0
 
-while number != 1:
-    max_factor = int(number**0.5) + 1
-    for factor in range(2, max_factor): 
-        if number % factor == 0: 
-            x = number / factor
-            number -= x
-            cost += number / x 
-            break
+def find_smallest_factor(number):
+    max_factor = int(number**0.5) + 1 #largest number we need to go to before concluding number is prime
+    if number % 2 == 0: return 2
+    for factor in range(3, max_factor, 2): 
+        if number % factor == 0:  return factor
+
+while number != 1: # 1 is what we're trying to get to
+    factor = find_smallest_factor(number)
+    if factor:
+        leftover = number / factor #everything that is left after the smallest factor is divided out
+        number -= leftover #number can be reduced by this because 
+        cost += number / leftover
     else: # if number is prime, go back only 1
         number -= 1
         cost += number
+    print number, factor, cost
         
 print cost
