dd0af0a1f49010631e799a72370f520980c4ef50
BorisVerk <boris.verk@gmail.com>
2014-02-24 23:05:06 -0700

    more formatting

--- contests/2013/a4.py
+++ contests/2013/a4.py
@@ -1,14 +1,12 @@
 
 
 def is_taller(taller_person, shorter_person, search_tree):
-
     """
     returns 'yes' if taller_person is taller than shorter_person
     returns 'no' if shorter_person turns out to be the taller person
     returns 'unknown' if it cannot be determined (as per the problem's specifications)
     uses a Breadth first Search through the "search_tree"
     """
-
 
     # visited is a list for tracking which people have already been searched
     # (so that we don't go to same guy more than once and get stuck in a loop)
