ca1a635e6d151d122e543a501d2651f3829ef7b7
BorisVerk <boris.verk@gmail.com>
2014-02-25 02:00:13 -0700

    formatting 1.py

--- contests/2012/1.py
+++ contests/2012/1.py
@@ -1,5 +1,3 @@
-#started this year at 2225
-# note to self: Need a refresher on perms and esspecially combinations
 with open('s1.in', 'r') as infile:
-    jersey_num = int(infile.readline().strip()) - 1
-print (jersey_num * (jersey_num-1) * (jersey_num-2))/6
+    jersey_num = int(infile.readline()) - 1
+print (jersey_num * (jersey_num-1) * (jersey_num-2))/6 #combinations, man.
