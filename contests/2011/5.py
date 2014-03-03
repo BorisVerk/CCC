2754a4d2780cc8ef06cf49b7e0f185f1fa6afa33
BorisVerk <boris.verk@gmail.com>
2014-03-03 02:03:51 -0700

    actually deleted unfinished solutions

--- contests/2011/5.py
+++
@@ -1,6 +0,0 @@
-#unfinished
-with open('s5.in', 'r') as infile:
-    infile.readline() #dont need the first line
-    remaining_file_lines = infile.read().split('\n')
-    switches = [bool(int(x)) for x in remaining_file_lines]
-
