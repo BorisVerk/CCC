2598b0037c8f31e52fde05d1ab8a1aaa96173384
BorisVerk <boris.verk@gmail.com>
2014-03-03 01:56:15 -0700

    trying to delete uncompleted files

--- contests/2014/3.py
+++ contests/2014/3.py
@@ -1,24 +1,20 @@
-#not completed
-
 def throw_in_lake(mountain):
     branch = []
+    current_car = min(mountain)
 
-    current_car = 1
-    while mountain:
-        car = mountain.pop()
-        if car == current_car:
-            continue #Throw it in the lake
-        elif car == (current_car + 1) and current_car not in mountain:
-            current_car += 1
-        else:
-            branch.append(car)
+    while len(mountain) + len(branch) > 0:
+        if current_car
 
-tests = int(infile.readline())
+    return 'Y'
 
-for test in range(tests):
-    len_cars = int(infile.readline())
-    mountain = []
-    for _ in range(len_cars):
-        car = int(infile.readline())
-        mountain.append(car)
-    print throw_in_lake(mountain)
+
+with open('s3.in', 'r') as infile:
+    tests = int(infile.readline())
+
+    for test in range(tests):
+        len_cars = int(infile.readline())
+        mountain = []
+        for _ in range(len_cars):
+            car = int(infile.readline())
+            mountain.append(car)
+        print throw_in_lake(mountain)
